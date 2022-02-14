ce349167ee5b11c803217bcbe40c613f3fb2feff
vcjimenez1 <vcjimenez1@espe.edu.ec>
2022-02-13 22:00:00 -0500

    NewStandardization

---
+++ python/efitoggler.py
@@ -0,0 +1,152 @@
+from sys import platform
+from sys import stdout, version_info
+from time import sleep
+import os
+import subprocess
+
+def clear(): os.system('clear')
+
+LANGUAGE=0
+
+def main():
+    clear(); language(); cover(); verify(); toggle()
+
+def printer(type, position):
+    
+    GREEN = '\033[92m';  WARNING = '\033[93m'; FAIL = '\033[91m';  ENDC = '\033[0m';
+  
+    DICTIONARY_ENG=[
+		"Your Operating System is not macOS, exiting",
+		"All dependencies is ok!",
+		"EFI Folder is mounted, unmounting",
+		"EFI Folder is not mounted, mounting",
+		"Done!",
+        "Your Python versión is less than 3.5, exiting"
+    ]
+    DICTIONARY_ESP=[
+		"Tu sistema operativo no es macOS, saliendo",
+		"¡Todo ok!",
+		"La carpeta EFI esta montada, desmontando",
+		"La carpeta EFI no esta montada, montando",
+		"¡Listo!",
+        "Tu versión de Python es menor que 3.5, saliendo"
+    ]	
+    
+    if LANGUAGE == 1:
+         if type == "print": print(f"{DICTIONARY_ENG[position]}")
+         elif type == "info": print(f"[{GREEN}+{ENDC}] INFO: {DICTIONARY_ENG[position]}")
+         elif type == "warn": print(f"[{WARNING}*{ENDC}] WARNING: {DICTIONARY_ENG[position]}")
+         elif type == "error": print(f"[{FAIL}!{ENDC}] ERROR: {DICTIONARY_ENG[position]}")
+         else: print(f"[?] UNKNOWN: {DICTIONARY_ENG[position]}")
+    else:
+        if type == "print": print(f"{DICTIONARY_ESP[position]}")
+        elif type == "info": print(f"[{GREEN}+{ENDC}] INFO: {DICTIONARY_ESP[position]}")
+        elif type == "warn": print(f"[{WARNING}*{ENDC}] WARNING: {DICTIONARY_ESP[position]}")
+        elif type == "error": print(f"[{FAIL}!{ENDC}] ERROR: {DICTIONARY_ESP[position]}")
+        else: print(f"[?] UNKNOWN: {DICTIONARY_ESP[position]}")        
+
+def language(): 
+    
+    global LANGUAGE
+    
+    print("Bienvenido /  Welcome")
+    print("Please, choose your language / Por favor selecciona tu idioma")
+    print("1) English")
+    print("2) Espanol")
+    
+    option=char()
+  
+    if option == "1":
+        LANGUAGE=1
+    elif option == "2":
+        LANGUAGE=2
+    else:
+        exit(1)
+
+def cover():
+    print(r'''                                     `"~>v??*^;rikD&MNBQku*;`                                           ''')
+    print(r'''                                `!{wQNWWWWWWWWWWWWWWWNWWWWWWNdi^`                                       ''')
+    print(r'''                              .v9NWWWWNRFmWWWWWWWWWWWWga?vs0pNWWWMw!                                    ''')
+    print(r'''                            !9WWWWWWU>`>&WWWWWWUH!_JNWWWWWQz  ^EWWWWg|                                  ''')
+    print(r'''                           _SWWWWWNe: /RWWWWWWNNHBRuyix&WWWWWg2?-"VNWWW6_                               ''')
+    print(r'''                         "kWWWWWNz. .zNWWWWWWw=, ^NsLQNW**MWWWW&WQJuNWWWNr.                             ''')
+    print(r'''                       .FNWWWWNu. rL&WWWWWWg!!*;^Jo!*BN0aFx)>|!;;;;;!~\r)xFwaao?|,                      ''')
+    print(r'''                     .sNWWWWMi` -,#WWWWWWNi"` Siwu UWv  .;^|^;`               .!*lUSF*;                 ''')
+    print(r'''                    )BWWWWWo.   9NWWWWWW0; ;PvLc*aU&^ |L=-``.;>*=                   ;)wmkL_             ''')
+    print(r'''                  _QWWWWWq"   .aWWWWWWWs`  rF<>\^gQ, /i   ,;;.  !2                      ,*k0F\`         ''')
+    print(r'''                 *NWWWWNv   ,/&WWWWWWNr "!SL92l)BU.  ^x   x. L,  I_                        `>P&F;       ''')
+    print(r'''               `2WWWWWg;    !BWWWWWWD"   .s;!\xNa     /L,   !L`  P,                           .?&gr     ''')
+    print(r'''              ,QWWWWWS`  >;LWWWWWWWk`_;!\u|  ^Ml        ;~!^,  `iv                              `?Ng^   ''')
+    print(r'''             ^BWWWWWi   *i7NWWWWWWc "a;;?ii"~NV             `;?},                                 ,9WF  ''')
+    print(r'''            >WWWWWB!  ` ;8WWWWWWM=  r>`;F/2wNc          .;||!,                                      oW#.''')
+    print(r'''           ?WWWWW#"  `2;7NWWWWW&_ =_=u%ir`>Wi                                                        PW6''')
+    print(r'''          rWWWWWc   `||>WWWWWWU.  r^?7;!v*W)                                                         ,WW|''')
+    print(r'''         ^NWWWB!  ! \jrmWWWWWw  `vL.k*\vkW$>rr*r;`        ;rL{7)>!`                                   mWF''')
+    print(r'''        .BWWW$,   ,u. PWWWWW) ,r`)|)!__LWv     `;L"     |s>:```._|JuL                                 qWE''')
+    print(r'''        uWWWH` .vi"Fo*WWWWN>   ^v  r*`>W}                                                             &Ws ''')
+    print(r'''       ;WWWP`  `=*ox_pWWWB; ^)i`9xr,#7W*            .     ,\*`                                       |WW! ''')
+    print(r'''       SWWD` >LLr^_y*NWWQ"  ,<?P~|iF0W}            ~;   v_ `o;                                      .0WU''')
+    print(r'''      ^WW0,.!F2xULFi5WW0` >7vr!!z_`*Wv             `|;;^!,~!`                                      .8W8.''')
+    print(r'''      dWN;`>JyrkIr`!NWN! ,uFia!9?*2WI                                                             ;QWD.''')
+    print(r'''     =WW7`_S)~Fxv| xWWi ;}drqa=;=uWRNmL,                                                         rWWt`  ''')
+    print(r'''     DWP`;LiL;}c*rsWW&`,Po_e7L/ =Nc `>oD$aaw%ouic7)*r>=|^^~!;;;;;;;;;;;;;~^\>rvL{JctxiiiiuusoF2kgBS/  ''')
+    print(r'''    ;WN\\Uy>*rF.,pWWWr-;?J"vov^^Nu         `.,"_;!~^\=>r*v?LL{}Jjjjjjj}}7?vr>\^!;____-""",,,..``    ''')
+    print(r'''    iW?_**>^;>"~&EeWg=|liv*s!~?NL''')
+    print(r'''    wWc*$>*~~L6Ni QW! \Uursx >WJ''')
+    print(r'''    2M)o*_F "R0; .Wd~U7,``;*iN>''')
+    print(r'''    xWe?vI7cMu`  ,W&>xssr~=PB|''')
+    print(r'''    "W% ,cBZ_    `M2l\/i,,QQ,''')
+    print(r'''     |U$di_       UBu>i)yBy`''')
+    print(r'''                  ^Wx,rDR!''')
+    print(r'''                   \ZUl^''')
+    print(r'''.oPYo. .oPYo. .pPYo.   .oPYo.                       o   o                 .oPYo.   o              8  o                ''')
+    print(r'''8  .o8     `8 8        8    8                       8                     8        8              8                   ''')
+    print(r'''8 .P`8   .oP` 8oPYo.   8      oPYo. .oPYo. .oPYo.  o8P o8 o    o .oPYo.   `Yooo.  o8P o    o .oPYo8 o8 .oPYo. .oPYo.  ''')
+    print(r'''8.d` 8    `b. 8`  `8   8      8  `` 8oooo8 .oooo8   8   8 Y.  .P 8oooo8       `8   8  8    8 8    8  8 8    8 Yb..   ''')
+    print(r'''8o`  8     :8 8.  .P   8    8 8     8.     8    8   8   8 `b..d` 8.            8   8  8    8 8    8  8 8    8   `Yb. ''')
+    print(r'''`YooP` `YooP` `YooP`   `YooP` 8     `Yooo` `YooP8   8   8  `YP`  `Yooo`   `YooP`   8  `YooP` `YooP`  8 `YooP` `YooP. ''')
+    print(r''':.....::.....::.....::::.....:..:::::.....::.....:::..::..::...:::.....::::.....:::..::.....::.....::..:.....::.....:''')
+    print(r''':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::''')
+    print(r''':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::''')
+
+def verify():
+    if platform != "darwin":
+       clear(); printer("error",0); exit(1)
+
+    if version_info < (3, 5):
+        clear(); printer("error",5); exit(1)
+        
+    printer("print", 1)
+
+    spinner = spinning()
+    for _ in range(15):
+        stdout.write(next(spinner))
+        stdout.flush()
+        sleep(0.1)
+        stdout.write('\b')
+    
+def toggle():
+     SUB_EFIPART=subprocess.Popen("diskutil list | sed -ne '/EFI/p' | sed -ne 's/.*\(d.*\).*/\1/p'", 
+                                  shell=True, stdout=subprocess.PIPE)
+     EFIPART = SUB_EFIPART.stdout.read()
+     
+
+def char():
+    import tty, sys, termios  
+    fd = sys.stdin.fileno()
+    oldSettings = termios.tcgetattr(fd)
+    try:
+        tty.setcbreak(fd)
+        answer = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, oldSettings)
+    return answer
+
+def spinning():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
+if __name__ == "__main__":
+    main()
+    
