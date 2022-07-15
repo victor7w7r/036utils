0f518f4d2c97517d3f83c89ea03676f7949dfe6a
Víctor Jiménez <43785784+victor7w7r@users.noreply.github.com>
2022-07-14 21:10:22 -0500

    ReadyEfiTogglerDart

--- dart/efitoggler/lib/utils.dart
+++ dart/efitoggler/lib/utils.dart
@@ -5,13 +5,16 @@ void clear() {
   print(Process.runSync("clear", [], runInShell: true).stdout);
 }
 
-void spin() {
+void spin(Function ready) {
   const charSet = ['|', '/', '-', '\\'];
   int cursor = 0;
   Timer.periodic(Duration(milliseconds: 100), (timer) {
     stdout.write('\r${charSet[cursor]}');
     cursor++;
     if(cursor == 4) cursor = 0;
-    if(timer.tick == 16) timer.cancel();
+    if(timer.tick == 16) {
+      timer.cancel();
+      ready();
+    }
   });
 }
