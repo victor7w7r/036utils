45f56b6fb46b631af89b3e81462f02f01b496708
Victor Jiménez <vcjimenez1@espe.edu.ec>
2022-07-16 11:35:32 -0500

    ext4pyToInquirer

--- python/ext4-optimizer.py
+++ python/ext4-optimizer.py
@@ -1,60 +1,62 @@
+from statistics import quantiles
 from subprocess import call, PIPE, Popen
-from sys import stdin, stdout, platform, version_info
+from sys import stdout, platform, version_info
 from os import system
-from termios import tcgetattr, tcsetattr, TCSADRAIN
 from time import sleep
-from tty import setcbreak
-from dialog import Dialog
+from pip import main
 
-d = Dialog(dialog="dialog")
-d.set_background_title("036 Creative Studios")
+try:
+    inquirer = __import__('inquirer')
+except ImportError:
+    main(['install', 'inquirer'])
+    inquirer = __import__('inquirer')
 
 LANGUAGE: int = 0
 
-def main() -> None: utils.clear(); language(); cover(); verify(); menu()
+def core() -> None: utils.clear(); language(); cover(); verify(); menu()
 
 def printer(type: str, position: int) -> None:
-    
+
     GREEN = '\033[92m';  WARNING = '\033[93m'; FAIL = '\033[91m';  ENDC = '\033[0m'
 
     DICTIONARY_ENG=(
-	"Your Operating System is not GNU/Linux, exiting",
-	"In this system the binary sudo doesn't exist.",
-	"e4defrag binary is not present in this system, please install",
-	"fsck.ext4 is not present in this system, please install",
-	"The dialog binary is not available in this system, please install",
-	"All dependencies is ok!",
-	"There's not ext4 partitions available, only works with USB devices",
-	"All the ext4 partitions are mounted in your system, please unmount the desired partition to optimize",
-	"=============== VERIFY FILESYSTEM ERRORS =============== \n",
-	"=============== FAILURE =============== \n",
-	"=============== OK =============== \n",
-	"=============== OPTIMIZE FILESYSTEM =============== \n",
-	"=============== DEFRAG FILESYSTEM, PLEASE WAIT =============== \n",
-	"=============== LAST VERIFY FILESYSTEM =============== \n",
-    "Your Python versión is less than 3.5, exiting",
-    "You need to be root, execute with sudo"
+        "Your Operating System is not GNU/Linux, exiting",
+        "In this system the binary sudo doesn't exist.",
+        "e4defrag binary is not present in this system, please install",
+        "fsck.ext4 is not present in this system, please install",
+        "The dialog binary is not available in this system, please install",
+        "All dependencies is ok!",
+        "There's not ext4 partitions available, only works with USB devices",
+        "All the ext4 partitions are mounted in your system, please unmount the desired partition to optimize",
+        "=============== VERIFY FILESYSTEM ERRORS =============== \n",
+        "=============== FAILURE =============== \n",
+        "=============== OK =============== \n",
+        "=============== OPTIMIZE FILESYSTEM =============== \n",
+        "=============== DEFRAG FILESYSTEM, PLEASE WAIT =============== \n",
+        "=============== LAST VERIFY FILESYSTEM =============== \n",
+        "Your Python versión is less than 3.5, exiting",
+        "You need to be root, execute with sudo"
     )
-    
+
     DICTIONARY_ESP=(
         "Este sistema no es GNU/Linux, saliendo",
-	"En este sistema no existe el binario de superusuario.",
-	"El ejecutable e4defrag no está presente en tu sistema, por favor instalalo",
-	"fsck.ext4 no está presente en tu sistema, por favor instalalo",
-	"dialog no está presente en tu sistema, por favor instalalo",
-	"Todo ok!",
-	"No hay particiones ext4 disponibles en el sistema, solofunciona con dispositivos USB",
-	"Todas las particiones ext4 estan montadas en el sistema, por favor desmontar la particion deseada para optimizar",
-	"=============== VERIFICAR ERRORES EN EL SISTEMA DE ARCHIVOS =============== \n",
-	"=============== FALLA =============== \n",
-	"=============== LISTO =============== \n",
-	"=============== OPTIMIZAR EL SISTEMA DE ARCHIVOS =============== \n",
-	"=============== DESFRAGMENTAR EL SISTEMA DE ARCHIVOS, ESPERE POR FAVOR =============== \n",
-	"=============== VERIFICAR POR ULTIMA VEZ EL SISTEMA DE ARCHIVOS =============== \n",
-    "Tu versión de Python es menor que 3.5, saliendo",
-    "Necesitas ser superusuario, ejecuta con sudo"
+        "En este sistema no existe el binario de superusuario.",
+        "El ejecutable e4defrag no está presente en tu sistema, por favor instalalo",
+        "fsck.ext4 no está presente en tu sistema, por favor instalalo",
+        "dialog no está presente en tu sistema, por favor instalalo",
+        "Todo ok!",
+        "No hay particiones ext4 disponibles en el sistema, solo funciona con dispositivos USB",
+        "Todas las particiones ext4 estan montadas en el sistema, por favor desmontar la particion deseada para optimizar",
+        "=============== VERIFICAR ERRORES EN EL SISTEMA DE ARCHIVOS =============== \n",
+        "=============== FALLA =============== \n",
+        "=============== LISTO =============== \n",
+        "=============== OPTIMIZAR EL SISTEMA DE ARCHIVOS =============== \n",
+        "=============== DESFRAGMENTAR EL SISTEMA DE ARCHIVOS, ESPERE POR FAVOR =============== \n",
+        "=============== VERIFICAR POR ULTIMA VEZ EL SISTEMA DE ARCHIVOS =============== \n",
+        "Tu versión de Python es menor que 3.5, saliendo",
+        "Necesitas ser superusuario, ejecuta con sudo"
     )
-    
+
     if LANGUAGE == 1:
         if type == "print": print(f"{DICTIONARY_ENG[position]}")
         elif type == "info": print(f"[{GREEN}+{ENDC}] INFO: {DICTIONARY_ENG[position]}")
@@ -69,9 +71,9 @@ def printer(type: str, position: int) -> None:
         else: print(f"[?] UNKNOWN: {DICTIONARY_ESP[position]}")
 
 def reader(position: int) -> str:
-    
+
     DICTIONARY_ENG=(
-		"Choose a Option\n", 
+		"Choose a Option",
 		"Optimize a ext4 partition",
 		"Exit to the shell",
 		"Please select a partition" ,
@@ -81,7 +83,7 @@ def reader(position: int) -> str:
 	)
 
     DICTIONARY_ESP=(
-		"Seleccione una opcion\n",
+		"Seleccione una opcion",
 		"Optimizar una particion de tipo ext4",
 		"Salir al shell",
 		"Por favor selecciona una partition",
@@ -97,19 +99,23 @@ def commandverify(cmd: str) -> bool:
     return call("type " + cmd, shell=True, stdout=PIPE, stderr=PIPE) == 0
 
 def language() -> None:
-    
+
     global LANGUAGE
-    
-    print("Bienvenido / Welcome")
-    print("Please, choose your language / Por favor selecciona tu idioma")
-    print("1) English"); print("2) Espanol")
-    option: str = utils.char()
-    if option == "1": LANGUAGE=1
-    elif option == "2": LANGUAGE=2
-    else: exit(1)
+
+    print('Welcome / Bienvenido')
+
+    q = [
+        inquirer.List('language',
+            message='Please, choose your language / Por favor selecciona tu idioma',
+            choices=['English', 'Espanol'])
+    ]
+
+    data = inquirer.prompt(q)
+    if data['language'] == 'English': LANGUAGE = 1
+    else: LANGUAGE = 2
 
 def cover() -> None:
-    
+
     utils.clear()
     print(r'''                                     `"~>v??*^;rikD&MNBQku*;`                                           ''')
     print(r'''                                `!{wQNWWWWWWWWWWWWWWWNWWWWWWNdi^`                                       ''')
@@ -168,21 +174,21 @@ def verify() -> None:
         utils.clear(); printer("error",3); exit(1)
     if not commandverify("dialog"):
         utils.clear(); printer("error",4); exit(1)
-    ext4listener(); printer("print",5)    
+    ext4listener(); printer("print",5)
     spinner = utils.spinning()
     for _ in range(15):
         stdout.write(next(spinner))
-        stdout.flush(); sleep(0.1)  
+        stdout.flush(); sleep(0.1)
         stdout.write('\b')
     utils.clear()
-    
-def ext4listener(menuable: str = "", echoparts: str = "") -> list[str, str]:
-    
+
+def ext4listener(menuable: str = "", echoparts: str = "") -> list[str]:
+
     COUNT: int = 0; EXTCOUNT: int = 0; MOUNTCOUNT: int = 0
     ABSOLUTEPARTS: str = ""; DIRTYDEVS: list = []
     EXTPARTS: list = []; PARTS: list = []
     UMOUNTS: list[str, str] = []
-    
+
     ROOT: str = Popen(r"""df -h | sed -ne '/\/$/p' | cut -d" " -f1
                         """, shell=True, stdout=PIPE).stdout.read().decode('utf-8').replace("\n", "")
     VERIFY: str = Popen(r"""find /dev/disk/by-id/ | sort -n | sed 's/^\/dev\/disk\/by-id\///'
@@ -191,110 +197,105 @@ def ext4listener(menuable: str = "", echoparts: str = "") -> list[str, str]:
     for DEVICE in VERIFY:
         DIRTYDEVS.append(Popen(f'readlink "/dev/disk/by-id/{DEVICE}"', shell=True, stdout=PIPE)
                         .stdout.read().decode('utf-8').rstrip().split("\n")[0])
-        
+
     DIRTYDEVS = list(filter(('').__ne__, DIRTYDEVS))
 
     for DEV in DIRTYDEVS:
         ABSOLUTEPARTS = Popen(f"""
-                            echo {DEV} | sed 's/^\.\.\/\.\.\//\/dev\//' | sed '/.*[[:alpha:]]$/d' | sed '/blk[[:digit:]]$/d'""", 
+                            echo {DEV} | sed 's/^\.\.\/\.\.\//\/dev\//' | sed '/.*[[:alpha:]]$/d' | sed '/blk[[:digit:]]$/d'""",
                             shell=True, stdout=PIPE).stdout.read().decode('utf-8').rstrip()
         if ABSOLUTEPARTS != "":
             if ABSOLUTEPARTS != ROOT:
-                PARTS.append(Popen(f"echo {DEV} | sed 's/^\.\.\/\.\.\///' | sed '/.*[[:alpha:]]$/d' | sed '/blk[[:digit:]]$/d'", 
+                PARTS.append(Popen(f"echo {DEV} | sed 's/^\.\.\/\.\.\///' | sed '/.*[[:alpha:]]$/d' | sed '/blk[[:digit:]]$/d'",
                                     shell=True, stdout=PIPE).stdout.read()
-                                    .decode('utf-8').rstrip().split("\n")[0]); COUNT += 1    
+                                    .decode('utf-8').rstrip().split("\n")[0]); COUNT += 1
 
     for PART in PARTS:
         TYPE: str = Popen(f'''lsblk -f /dev/{PART} | sed -ne '2p' | cut -d " " -f2''',
                     shell=True, stdout=PIPE).stdout.read().decode('utf-8').rstrip()
-        if TYPE == "ext4": EXTCOUNT += 1; EXTPARTS.append(PART)     
-    
+        if TYPE == "ext4": EXTCOUNT += 1; EXTPARTS.append(PART)
+
     if (EXTCOUNT == 0):
         utils.clear(); printer("error",6)
         if menuable == "menu" : input(reader(4)); return
         else: exit(1)
-        
+
     for PARTITIONSDEF in EXTPARTS:
-        MOUNTED: str = Popen(f"lsblk /dev/{PARTITIONSDEF} | sed -ne '/\//p'", 
+        MOUNTED: str = Popen(f"lsblk /dev/{PARTITIONSDEF} | sed -ne '/\//p'",
                             shell=True, stdout=PIPE).stdout.read().decode('utf-8').rstrip()
         if MOUNTED != "": MOUNTCOUNT += 1
-        else: UMOUNTS.append([f"/dev/{PARTITIONSDEF}","ext4"])
-        
+        else: UMOUNTS.append(f"/dev/{PARTITIONSDEF}")
+
     if MOUNTCOUNT == EXTCOUNT :
         utils.clear(); printer("error",7)
         if(menuable == "menu"): input(reader(4)); return
         else: exit(1)
-        
+
     if echoparts == "print": return UMOUNTS
-        
+
 def menu() -> None:
-    
-    choices = [(reader(5),reader(1)),(reader(6),reader(2))]
-    response = d.menu(reader(0), 15, 60, 4, choices)
-    if(response[0] == "ok" and response[1] == reader(5)):
-        defragmenu()
-    else: exit(0)
+
+    q = [
+        inquirer.List('menu',
+            message=reader(0),
+            choices=[reader(1), reader(2)])
+    ]
+    answers = inquirer.prompt(q)
+    if(answers['menu'] == reader(1)):
+        utils.clear(); defragmenu()
+    else: utils.clear(); exit(0)
 
 def defragmenu() -> None:
-    
+
+    q = [
+        inquirer.List('menu',
+            message=reader(0),
+            choices=ext4listener("menu","print"))
+    ]
+    answers = inquirer.prompt(q)
     utils.clear()
-    choices = ext4listener("menu","print")
-    response = d.menu(reader(0), 15, 50, 4, choices)
-    if(response[0] == "ok"):
-        defragaction(response[1])
-    else: exit(0)
+    defragaction(answers['menu'])
 
 def defragaction(part: str) -> None:
-    
+
     utils.clear()
     if part == "": return
-    
+
     printer("print",8)
-    
+
     if call("sudo cat < /dev/null", shell=True) == 0:
         if call(f"sudo fsck.ext4 -y -f -v {part}", shell=True) != 8:
             print(" "); printer("print",10); input(reader(4)); utils.clear()
         else: printer("print",9); input(reader(4)); menu(); return
-    
+
         printer("print",11)
-        
+
         if call(f"sudo fsck.ext4 -y -f -v -D {part}", shell=True) != 8:
             print(" "); printer("print",10); input(reader(4)); utils.clear()
         else: printer("print",9); input(reader(4)); menu(); return
-            
-        system("mkdir /tmp/optimize 2> /dev/null") 
-        system(f"sudo mount {part} /tmp/optimize") 
-        
+
+        system("mkdir /tmp/optimize 2> /dev/null")
+        system(f"sudo mount {part} /tmp/optimize")
+
         printer("print",12); system(f"sudo e4defrag -v {part}")
         print(" ")
         system(f"sudo umount {part}"); printer("print",10)
         input(reader(4)); utils.clear()
-        
+
         printer("print",13)
-        
+
         if call(f"sudo fsck.ext4 -y -f -v {part}", shell=True) != 8:
             print(" "); printer("print",10); input(reader(4)); utils.clear(); menu()
         else: printer("print",9); input(reader(4)); menu(); return
     else: printer("print",9); input(reader(4)); menu(); return
-    
+
 class utils:
-    
+
     def clear() -> None: system('clear')
-    
-    def char() -> str:
-        fd = stdin.fileno()
-        oldSettings = tcgetattr(fd)
-        try:
-            setcbreak(fd)
-            answer = stdin.read(1)
-        finally:
-            tcsetattr(fd, TCSADRAIN, oldSettings)
-        return answer
-    
+
     def spinning():
         while True:
             for cursor in '|/-\\':
                 yield cursor
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": core()
